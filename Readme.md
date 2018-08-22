be3affe83ad72decd399f3656825814e343abbaa
Jan Topiński <jan.topinski@in4mates.com>
2018-08-22 11:02:10 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+##Instalacja
+
+`gem install`
+
+`bundle install`
+
+##Użycie
+
+Pobranie listy obiektów
+`get_obj_list <usr> <pass>`
+
+Pobranie statusów obiektów dla listy z csv
+`get_obj_status <usr> <pass>`
---
+++ get_obj_list.rb
@@ -0,0 +1,13 @@
+require 'savon'
+require 'csv'
+
+client = Savon.client(basic_auth: [ARGV[0], ARGV[1]],wsdl: "http://monitoring.pl/s115/LogisticWebService/LogisticWebService.asmx?wsdl")
+
+response = client.call(:get_obj_list, message: { criterion: ""})
+
+obj_list = response.body[:get_obj_list_response][:get_obj_list_result][:logistic_db_obj_data]
+CSV.open("obj_list.csv", "wb", headers: obj_list.first.keys, :write_headers => true) do |csv|
+  obj_list.each do |obj|
+    csv << obj.values
+  end
+end
